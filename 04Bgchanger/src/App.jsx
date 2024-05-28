e5eeae5d07af8d7c481e85c7ac869017873d8ba2
saurabsharma111 <souravsharma200333@gmail.com>
2024-05-28 09:37:24 +0530

    bgChanger

---
+++ 04Bgchanger/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+function App() {
+  const [color, setColor] = useState()
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200"
+      style={{background:color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button onClick={()=>setColor("red")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"red"}}>Red</button>
+            <button onClick={()=>setColor("green")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"green"}}>Green</button>
+            <button onClick={()=>setColor("blue")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"blue"}}>Blue</button>
+            <button onClick={()=>setColor("white")} className="outline-none px-4 py-1 rounded-full 
+            text-black shadow-lg" style={{background:"white"}}>White</button>
+            <button onClick={()=>setColor("black")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"black"}}>Black</button>
+            <button onClick={()=>setColor("pink")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"pink"}}>Pink</button>
+            <button onClick={()=>setColor("yellow")} className="outline-none px-4 py-1 rounded-full 
+            text-black shadow-lg" style={{background:"yellow"}}>Yellow</button>
+            <button onClick={()=>setColor("grey")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"Grey"}}>Grey</button>
+            <button onClick={()=>setColor("olive")} className="outline-none px-4 py-1 rounded-full 
+            text-white shadow-lg" style={{background:"olive"}}>Olive</button>
+          
+          </div>
+
+
+        
+        </div>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
