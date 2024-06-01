a286ccc1842522e7a97751749699f91a0e21117d
saurabsharma111 <souravsharma200333@gmail.com>
2024-06-01 13:35:24 +0530

    Context Api Project

---
+++ 08MiniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Hi! Context API </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
