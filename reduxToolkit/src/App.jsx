08020fe257ff20ba4084766d1fe19928e812edf9
saurabsharma111 <souravsharma200333@gmail.com>
2024-06-05 08:29:18 +0530

    ReduxToolkit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodos'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
