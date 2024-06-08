772695391aac217981f75feda080a5543f1e710a
saurabsharma111 <souravsharma200333@gmail.com>
2024-06-08 22:45:36 +0530

    Project setup using appwrite(Backend as a service)

---
+++ 12MEGABLOG/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        MegaBlog Website!
+      </h1>
+    </>
+  )
+}
+
+export default App
